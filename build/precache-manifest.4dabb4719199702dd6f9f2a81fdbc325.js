d53dc4283437c25d540a4af33b44a6f25c2b210e
Bhushan-shrirame <44223841+Bhushan-shrirame@users.noreply.github.com>
2022-10-18 22:58:59 +0530

    Add files via upload

---
+++ build/precache-manifest.4dabb4719199702dd6f9f2a81fdbc325.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93395cea6a1232654bb457c7c601b3cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67a3e907033b29123c0",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "277eef4860ed163cef00",
+    "url": "/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "f67a3e907033b29123c0",
+    "url": "/static/js/2.3275d752.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.3275d752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277eef4860ed163cef00",
+    "url": "/static/js/main.2a3e9654.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
